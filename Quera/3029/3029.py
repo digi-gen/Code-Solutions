3edd6df09b26d99923a12cb4ead22ea22e9fc1ed
PooyaAbbasi <poy.8383.abs@gmail.com>
2024-04-24 21:58:34 +0330

    adding docs

--- Quera/3029/3029.py
+++ Quera/3029/3029.py
@@ -3,6 +3,7 @@
 x, y = map(int, input().split())
 x1, y1 = map(int, input().split())
 
+# Just determining is the x of point of his friend is more or less than his x.
 if x > x1:
     print('Left')
 else:
