b15de529006a7b7dc878f6d51d1cf85a33e7ad5c
PooyaAbbasi <poy.8383.abs@gmail.com>
2024-07-10 16:38:02 +0330

    test: write test for readme updator QueraQuestion class

---
+++ Quera/readme_updator_test.py
@@ -0,0 +1,28 @@
+import unittest
+from readme_updator import *
+
+
+test_link = 'https://quera.org/problemset/9024'
+
+
+class QueraQuestionTestCase(unittest.TestCase):
+
+    question_test_instance = QueraQuestion(test_link)
+
+
+    def test_quera_question(self):
+        quera_question = QueraQuestion(question_link=test_link)
+
+    def test_get_quera_id(self):
+        quera_id = QueraQuestionTestCase.question_test_instance.get_quera_id()
+        self.assertEqual(quera_id, '9024')
+
+    def test_get_question_tags(self):
+        question_tags = QueraQuestionTestCase.question_test_instance.get_tags()
+        self.assertEqual(question_tags, ['Dynamic Programming',
+        'Greedy',])
+
+
+
+if __name__ == '__main__':
+    unittest.main()
