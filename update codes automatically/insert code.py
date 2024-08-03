836c091575b7f8a7e6668f8a43301e2be86c81f2
Mahmood <mr.manmahmood@gmail.com>
2024-08-03 17:53:22 +0330

    update insert code

--- update codes automatically/insert code.py
+++ update codes automatically/insert code.py
@@ -27,8 +27,8 @@ def scrap_web(url):
     # Extract question name
     question_name = soup.select_one("div#__next h1.chakra-heading.css-1o1jsbj").text
     
-    # Extract question difficulty
-    question_difficulty = soup.select_one("div#__next span[class*='chakra-badge css-']").text
+    # Extract question difficultyiculty
+    question_difficultyiculty = soup.select_one("div#__next span[class*='chakra-badge css-']").text
     
     # Extract question id
     question_id = url.split('/')[-1]
@@ -37,30 +37,29 @@ def scrap_web(url):
     all_tags_elements = soup.select("div#__next div.chakra-stack.css-1ainr4z span[dir='rtl'].chakra-text.css-zvlevn")
     tags = [tag.text for tag in all_tags_elements if tag.text in correct_tags]
     
-    return [question_name, question_difficulty, 0, question_id, tags]
+    return [question_name, question_difficultyiculty, 0, question_id, tags]
 
 
 
 def insert_code(info, script, path):
+    global a
     
-    a = {'ساده':'Easy', 'متوسط':'Mid-level', 'سخت':'Hard',}
+    path_temp = os.path.join('.', 'Quera', a[info[1]], info[3])
+    if not os.path.exists(path_temp):
+        os.makedirs(path_temp)
     
-    if not os.path.exists(f'./Quera/{a[info[1]]}/{info[3]}/'):
-        os.makedirs(f'./Quera/{a[info[1]]}/{info[3]}/')
-    
-    
-    files_in_folder = os.listdir(f'./Quera/{a[info[1]]}/{info[3]}/')
+    files_in_folder = os.listdir(path_temp)
     if len(files_in_folder) >= 1:
         for i in files_in_folder:
-            
-            with open(f'./Quera/{a[info[1]]}/{info[3]}/{i}', 'r', encoding='utf-8') as f:
+
+            with open(os.path.join(path_temp, i), 'r', encoding='utf-8') as f:
                 t = f.readlines()
                 
             if script == t:
                 return
         
     text = ''.join(script)
-    with open(f'./Quera/{a[info[1]]}/{info[3]}/{info[3]}_{len(files_in_folder)+1}.py', 'w', encoding='utf-8') as f:
+    with open(os.path.join(path_temp, f'{info[3]}_{len(files_in_folder)+1}.py'), 'w', encoding='utf-8') as f:
         f.write(text)
     
     tags = ''
@@ -68,28 +67,22 @@ def insert_code(info, script, path):
         tags += f'`{i}` '
         
     # text = 'count, id, name(link to question), score(link to answer), tags'
-    text = f'-1 | {info[3]} | [{info[0]}](https://quera.org/problemset/{info[3]}) | [جواب](./Quera/{a[info[1]]}/{info[3]}/) | {tags}|\n'
+    text = f'-1 | {info[3]} | [{info[0]}](https://quera.org/problemset/{info[3]}) | [جواب]({path_temp}/) | {tags}|\n'
     
     update_csv(text, info[1], path)
 
 
 
-def update_csv(text, diff, path):
+def update_csv(text, difficulty, path):
+    global a
+    
     info = text.split(' | ')
     
-    if diff == 'ساده':
-        with open(f'{path}/update codes automatically/simple.txt', 'r', encoding='utf-8') as f:
-            read_in_file = f.readlines()
+    name = a[difficulty]
+    with open(os.path.join(path, f'{name}.txt'), 'r', encoding='utf-8') as f:
+        read_in_file = f.readlines()
             
-    elif diff == 'متوسط':
-        with open(f'{path}/update codes automatically/mid.txt', 'r', encoding='utf-8') as f:
-            read_in_file = f.readlines()
-            
-    elif diff == 'سخت':
-        with open(f'{path}/update codes automatically/hard.txt', 'r', encoding='utf-8') as f:
-            read_in_file = f.readlines()
-            
-            
+    
     read_in_file = [[j for j in i.split(' | ')] for i in read_in_file]
     read_in_file.append(info)
     read_in_file.sort(key=lambda x:int(x[1]))
@@ -101,32 +94,24 @@ def update_csv(text, diff, path):
     
     read_in_file = [' | '.join(i) for i in read_in_file]
     
-    if diff == 'ساده':
-        with open(f'{path}/update codes automatically//simple.txt', 'w', encoding='utf-8') as f:
+    name = a[difficulty]
+    with open(os.path.join(path, f'{name}.txt'), 'w', encoding='utf-8') as f:
             for item in read_in_file:
                 f.write(item)
-            
-    elif diff == 'متوسط':
-        with open(f'{path}/update codes automatically/mid.txt', 'w', encoding='utf-8') as f:
-            for item in read_in_file:
-                f.write(item)
-            
-    elif diff == 'سخت':
-        with open(f'{path}/update codes automatically/hard.txt', 'w', encoding='utf-8') as f:
-            for item in read_in_file:
-                f.write(item)
-        
 
 
 def update_table(path):
-    
+    global a
     read_in_file = []
     
-    for i in ['simple.txt', 'mid.txt', 'hard.txt']:
-        with open(f'{path}/update codes automatically/{i}', 'r', encoding='utf-8') as f:
+    for i in a.values():
+        with open(os.path.join(path, f'{i}.txt'), 'r', encoding='utf-8') as f:
             read_in_file.append(f.read())
 
-    with open('./README.md', 'r', encoding='utf-8') as f:
+    root_path = path.split('\\')
+    root_path = '\\'.join(root_path[0:len(root_path)-1])
+    
+    with open(os.path.join(root_path, 'README.md'), 'r', encoding='utf-8') as f:
         text = f.read()
         text = text.split('***\n')
         
@@ -146,21 +131,18 @@ def update_table(path):
     
     text = '***\n'.join(text)
     
-    with open('./README.md', 'w', encoding='utf-8') as f:
+    with open(os.path.join(root_path, 'README.md'), 'w', encoding='utf-8') as f:
         f.write(text)
     
     
 
     
 if __name__ == '__main__':
+    global a 
+    a = {'ساده':'Easy', 'متوسط':'Mid-level', 'سخت':'Hard'}
     
     
-    path = os.getcwd()
-    
-    try:
-        path.replace('update codes automatically/', '')
-    except:
-        pass
+    path = os.path.dirname(os.path.abspath(__file__))
     
     path_to_answer = input('Please enter path to your directory that all of your complete codes is there: ')
 
@@ -168,8 +150,8 @@ if __name__ == '__main__':
     for i in files_in_folder:
         read_in_files = []
         
-        for j in ['simple.txt', 'mid.txt', 'hard.txt']:
-            with open(f'{path}/update codes automatically/{j}', 'r', encoding='utf-8') as f:
+        for j in a.values():
+            with open(os.path.join(path, f'{j}.txt'), 'r', encoding='utf-8') as f:
                 read = f.readlines()
                 try:
                     temp = [(i.split(' | ')[1]+'.py') for i in read]
@@ -177,7 +159,7 @@ if __name__ == '__main__':
                 except:
                     pass
                 
-        with open(path_to_answer+'/'+i, 'r', encoding='utf-8') as f:
+        with open(os.path.join(path_to_answer ,i), 'r', encoding='utf-8') as f:
             script = f.readlines()
             
         url = 'https://quera.org/problemset/'+i[:len(i)-3]
